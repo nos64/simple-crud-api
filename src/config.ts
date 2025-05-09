04aaead196221cc955e7c5aa9214794053ad8d1f
Mikhail Nosov <manosov1984@gmail.com>
2025-05-09 22:44:01 +0300

    feat: implement usersService

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4000;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
